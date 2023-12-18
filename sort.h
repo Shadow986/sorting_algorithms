3bdc9a6e5e57fe74c0041d32fad4eb1e1f7c0f9b
Shadow986 <archie986jr@gmail.com>
2023-12-17 23:18:12 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,13 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+
 
 #endif
