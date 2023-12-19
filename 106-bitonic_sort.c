d0da14859ad8e0737efe1f18172b0bce2035286c
Shadow986 <archie986jr@gmail.com>
2023-12-18 22:05:50 -0800

    106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * bitonic_merge - Merges two subarrays of an array in bitonic order
+ * bitonic_merge - a function that merges two subarrays
+ * of an array in bitonic order
  * @array: The array to be sorted
  * @size: The size of the array
  * @start: The starting index of the subarray
@@ -10,32 +11,29 @@
  */
 void bitonic_merge(int *array, size_t size, size_t start, size_t count, int dir)
 {
-    size_t k, i, j;
-    int temp;
+	size_t k, i, j;
+	int temp;
 
-    if (count > 1)
-    {
-        size_t mid = count / 2;
-
-        for (k = start; k < start + mid; k++)
-        {
-            if (dir == (array[k] > array[k + mid]))
-            {
-                temp = array[k];
-                array[k] = array[k + mid];
-                array[k + mid] = temp;
-            }
-        }
-
-        bitonic_merge(array, size, start, mid, dir);
-        bitonic_merge(array, size, start + mid, mid, dir);
-    }
-
-    print_array(array, size);
+	if (count > 1)
+	{
+		size_t mid = count / 2;
+		for (k = start; k < start + mid; k++)
+		{
+			if (dir == (array[k] > array[k + mid]))
+			{
+				temp = array[k];
+				array[k] = array[k + mid];
+				array[k + mid] = temp;
+			}
+		}
+		bitonic_merge(array, size, start, mid, dir);
+		bitonic_merge(array, size, start + mid, mid, dir);
+	}
+	print_array(array, size);
 }
 
 /**
- * bitonic_sort_recursive - Recursive function for bitonic sort
+ * bitonic_sort_recursive - a function that recursive function for bitonic sort
  * @array: The array to be sorted
  * @size: The size of the array
  * @start: The starting index of the subarray
@@ -44,28 +42,26 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t count, int dir)
  */
 void bitonic_sort_recursive(int *array, size_t size, size_t start, size_t count, int dir)
 {
-    size_t mid;
+	size_t mid;
 
-    if (count > 1)
-    {
-        mid = count / 2;
-
-        bitonic_sort_recursive(array, size, start, mid, 1);
-        bitonic_sort_recursive(array, size, start + mid, mid, 0);
-
-        bitonic_merge(array, size, start, count, dir);
-    }
+	if (count > 1)
+	{
+		mid = count / 2;
+		bitonic_sort_recursive(array, size, start, mid, 1);
+		bitonic_sort_recursive(array, size, start + mid, mid, 0);
+		bitonic_merge(array, size, start, count, dir);
+	}
 }
 
 /**
- * bitonic_sort - Sorts an array of integers in ascending order using the Bitonic sort algorithm
+ * bitonic_sort - a function that sorts an array of integers
+ * in ascending order using the Bitonic sort algorithm
  * @array: The array to be sorted
  * @size: The size of the array
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-
-    bitonic_sort_recursive(array, size, 0, size, 1);
+	if (array == NULL || size < 2)
+		return;
+	bitonic_sort_recursive(array, size, 0, size, 1);
 }
