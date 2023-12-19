eb9a300d161fb6ad5585f1cf242a2f5c9c9c1353
Shadow986 <archie986jr@gmail.com>
2023-12-18 21:29:40 -0800

    107-quick_sort_hoare.c

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - Sorts an array of integers in ascending order
+ *                    using the Quick sort algorithm with Hoare partition scheme
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    hoare_partition(array, 0, size - 1, size);
+}
+
+/**
+ * hoare_partition - Partitions the array using the Hoare partition scheme
+ * @array: The array to be partitioned
+ * @low: The starting index of the partition
+ * @high: The ending index of the partition
+ * @size: Number of elements in the array
+ *
+ * Return: The index of the pivot element
+ */
+int hoare_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1)
+    {
+        do {
+            i++;
+        } while (array[i] < pivot);
+
+        do {
+            j--;
+        } while (array[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        swap(&array[i], &array[j]);
+        print_array(array, size);
+    }
+}
+
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
